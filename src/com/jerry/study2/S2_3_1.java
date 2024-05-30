c636353056e320ba20ef0c4e935aa5464c98bdb6
Jerry-0627 <mincand0627@gmail.com>
2024-05-30 16:55:59 +0900

    delete

--- src/com/jerry/study2/S2_3_1.java
+++
@@ -1,9 +0,0 @@
-package com.jerry.study2;
-
-public class S2_3_1 {
-	public static void main(String[] args) {
-		int[] ar = new int[3];
-
-		System.out.println();
-	}
-}
--- src/com/jerry/study3_1/Test.java
+++
@@ -1,7 +0,0 @@
-package com.jerry.study3_1;
-
-public class Test {
-
-	int level;
-
-}
