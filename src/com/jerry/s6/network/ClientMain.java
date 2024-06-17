843e2a4e8e1b65013f677e9c45365297ba55676b
Jerry-0627 <mincand0627@gmail.com>
2024-06-17 17:48:43 +0900

    6.17_class(8/8)_ServerClientDuo

--- src/com/jerry/s6/network/ClientMain.java
+++ src/com/jerry/s6/network/ClientMain.java
@@ -1,6 +1,9 @@
 package com.jerry.s6.network;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
@@ -8,33 +11,81 @@ import java.util.Scanner;
 
 public class ClientMain {
 	public static void main(String[] args) {
+		// @ 소캣은 한번만 만들면됨.
 		Socket socket = null;
+
+		// @ 보내는 역할을 하는 클래스 선언
 		OutputStream os = null;
 		OutputStreamWriter ow = null;
 		Scanner sc = new Scanner(System.in);
+
+		// @ 받는 역할을 하는 클래스 선언
+		InputStream is = null;
+		InputStreamReader ir = null;
+		BufferedReader br = null;
+
 		try {
+			// @ 보내는 역할을 하는 클래스의 변수 선언 후 실행
 			socket = new Socket("192.168.7.118", 8282);
 			System.out.println("서버와 연결 성공");
-			System.out.println("서버로 보낼 메세지 입력");
-			String str = sc.next();
-			System.out.println(str);
+			while (true) {
 
-			// 서버와 연결하려고 하는데, 서버에 대한 정보는 다 소켓에 있음. ,,, 내보낼 준비중
-			os = socket.getOutputStream();
-			ow = new OutputStreamWriter(os); // 문자
-			ow.write(str + "\r\n");// str을 내보내려 하느데, 버퍼에 담긴 것을 내보내려 한다.
-			ow.flush(); // 버퍼가 안비워질 수도 있기 때문에 강제로 비우는 것.
+				System.out.println("1. 학생 정보 리스트");
+				System.out.println("2. 세부 학생 정보");
+				System.out.println("3. 종료");
+				String str = sc.next();
+				// System.out.println(str);
+				// 서버와 연결하려고 하는데, 서버에 대한 정보는 다 소켓에 있음. ,,, 내보낼 준비중
+				os = socket.getOutputStream();
+				ow = new OutputStreamWriter(os); // 문자
+				ow.write(str + "\r\n");// str을 내보내려 하느데, 버퍼에 담긴 것을 내보내려 한다.
+				ow.flush(); // 버퍼가 안비워질 수도 있기 때문에 강제로 비우는 것.
+				if (str.toUpperCase().equals("3")) {
+					System.out.println("채팅을 종료합니다.");
+					break;
+				}
 
-			ow.close();
-			os.close();
-			socket.close();
-			sc.close();
+				// @ 받는 역할을하는 클래스의 변수 선언 후 실행.
+
+				is = socket.getInputStream();
+				ir = new InputStreamReader(is);
+				br = new BufferedReader(ir);
+
+				while (true) {
+					String msg = br.readLine();
+					if (msg == null) {
+						break;
+					}
+					System.out.println(msg);
+				}
+
+//				StringTokenizer stn = new StringTokenizer(msg, "-");
+//				while (stn.hasMoreTokens()) {
+//					for (int i = 0; i < 6; i++) {
+//						System.out.print(stn.nextToken() + "\t");
+//					}
+//					System.out.println("");
+//				}
+
+//				System.out.println(msg);
+//				if (msg.toUpperCase().equals("exit")) {
+//					System.out.println("채팅을 종료합니다.");
+//					break;
+//				}
+
+			}
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {// 예외가 발생하던 어니던 무조건 실행해라.
 			try {
+				// @ 연결을 지워줘야 메모리의 사용을 줄일 수 있음.
+				br.close();
+				ir.close();
+				is.close();
+				ow.close();
+				os.close();
 				socket.close();
 				sc.close(); // 소켓이 끝난 다음에 scanner close
 			} catch (IOException e) {
--- src/com/jerry/s6/network/ServerMain.java
+++ src/com/jerry/s6/network/ServerMain.java
@@ -4,50 +4,97 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ServerMain {
 	public static void main(String[] args) {
 		// Network
 		// ip:port => socket
 		// Network 통신은 소켓끼리 1대1 통신이다.(서버와 클라이언트) (클라이언트가 여러개여도 1대1 통신임)
+
+		// @ 소캣을 만들기 전에 선언해야 하는 클래스
 		ServerSocket ss = null;
+		// @ 소캣은 서버와 클라이언트에서 하나씩만 만들면됨.
 		Socket sc = null;
+
+		// @ 데이터를 받는 클래스 선언
 		InputStream is = null;
 		InputStreamReader ir = null;
 		BufferedReader br = null;
-		try {
-			ss = new ServerSocket(8282);
-			// ServerSocket은 열어주는 것.. 클라이언트와 서버가 통신하기 위해선 Socket이 필요함
-			// ServerSocket은 종이컵을 만들어주는것
-			// 서버를 열고 Client의 접속을 기다리는 중.
-			System.out.println("서버 실행 후 클라이언트 기다리기");
-			sc = ss.accept();
-			System.out.println("Client와 연결이 성공 되었습니다.");
-			is = sc.getInputStream(); // 소켓이 연결되면 아웃풋 인풋 스트림 하나씩 생김. 0과 1을 처리하는 애임
-			ir = new InputStreamReader(is); // 0과 1을 문자로 읽어주는애
-			br = new BufferedReader(ir); // 애는 빨대가 3개 꽂힌거임.
 
-			String msg = br.readLine();
+		// @ 데이터를 보내는 클래스 선언
+		OutputStream os = null;
+		OutputStreamWriter ow = null;
+		String str = null;
+		Scanner scan = new Scanner(System.in);
 
-			System.out.println(msg);
-
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
+		while (true)
 			try {
-				br.close();
-				ir.close();
-				is.close();
-				sc.close();
-				ss.close();
-			} catch (IOException e) {
+				ss = new ServerSocket(8282);
+				// ServerSocket은 열어주는 것.. 클라이언트와 서버가 통신하기 위해선 Socket이 필요함
+				// ServerSocket은 종이컵을 만들어주는것
+				// 서버를 열고 Client의 접속을 기다리는 중.
+				System.out.println("서버 실행 후 클라이언트 기다리기");
+				sc = ss.accept();
+				System.out.println("Client와 연결이 성공 되었습니다.");
+
+				while (true) {
+					// @ 데이터를 받는 변수 선언
+					is = sc.getInputStream(); // 소켓이 연결되면 아웃풋 인풋 스트림 하나씩 생김. 0과 1을 처리하는 애임
+					ir = new InputStreamReader(is); // 0과 1을 문자로 읽어주는애
+					br = new BufferedReader(ir); // 애는 빨대가 3개 꽂힌거임.
+					String msg = br.readLine();
+					System.out.println(msg);
+					if (msg.toUpperCase().equals("exit")) {
+						System.out.println("채팅을 종료합니다.");
+						break;
+
+					}
+//					File file = new File("c:\\study", "Client.txt");
+//					FileReader freader = new FileReader(file);
+//					BufferedReader breader = new BufferedReader(freader);
+//					String str1 = breader.readLine();
+//					if (msg.equals("1")) {
+//						ow.write(str1);
+//						ow.flush();
+//					}
+
+					// @ 데이터를 보낼 변수 선언
+					System.out.println("클라이언트로 보낼 메세지를 입력하세요.");
+					str = scan.next();
+					os = sc.getOutputStream();
+					ow = new OutputStreamWriter(os);
+					ow.write(str + "\r\n");
+					ow.flush();
+					if (str.toUpperCase().equals("exit")) {
+						System.out.println("채팅을 종료합니다.");
+						break;
+					}
+
+				}
+
+			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}
+			} finally {
+				try {
+					ow.close();
+					os.close();
+					br.close();
+					ir.close();
+					is.close();
+					sc.close();
+					ss.close();
 
-		}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
 	}
 }
